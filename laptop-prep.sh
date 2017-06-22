070e719a41705beb03ffbb1dc662950de3ba437a
Ryan Shoemaker <rshoemak@cisco.com>
2017-06-22 13:08:36 -0500

    added laptop prep script

---
+++ laptop-prep.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo pip install requests
+sudo yum -y update
+sudo yum install -y centos-release-openstack-newton
+sudo yum install -y python-ncclient
+cp DevNet2556/*.py /bootflash
+exit
