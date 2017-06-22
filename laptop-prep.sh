dbf42bbc2a0d2e0668c5dc4bf1284d61afb2061b
Ryan Shoemaker <rshoemak@cisco.com>
2017-06-22 13:14:05 -0500

    modified laptop prep script

--- laptop-prep.sh
+++ laptop-prep.sh
@@ -5,4 +5,5 @@ sudo yum -y update
 sudo yum install -y centos-release-openstack-newton
 sudo yum install -y python-ncclient
 cp DevNet2556/*.py /bootflash
+cp DevNet2556/IOS-config-base.txt /bootflash
 exit
